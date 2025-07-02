bbdc4bcf0be46b4ea4f768201e32ec1325df6382
dongig.shin <dongig.shin@samsung.com>
2025-07-02 10:04:21 +0900

    [feat] page1

--- Project100/prime-factors_test.cpp
+++ Project100/prime-factors_test.cpp
@@ -1,6 +1,11 @@
 #include "gmock/gmock.h"
 #include "prime-factors.cpp"
 
-TEST(PrimeFactors, PrimeTest) {
-	EXPECT_EQ(1, 1);
+using namespace std;
+
+TEST(PrimeFactors, Of1) {
+	PrimeFactors prime_factor;
+	vector<int> expected = {};
+	
+	EXPECT_EQ(expected, prime_factor.of(1));
 }
