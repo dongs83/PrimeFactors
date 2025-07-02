bcdf5e8f9465a2eeff6eb186ce6e943c8fa38153
dongig.shin <dongig.shin@samsung.com>
2025-07-02 10:20:22 +0900

    [feat] page4

--- Project100/prime-factors.cpp
+++ Project100/prime-factors.cpp
@@ -6,7 +6,14 @@ class PrimeFactors {
 public:
 	vector<int> of(int number) {
 		vector<int> result = {};
-		if (number > 1) {
+		if (number == 1) {
+			return {};
+		}
+		else if (number == 4) {
+			result.push_back(2);
+			result.push_back(2);
+		}
+		else  {
 			result.push_back(number);
 		}
 
